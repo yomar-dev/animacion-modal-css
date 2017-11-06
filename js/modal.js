295299879d4d397a9850d6382665202a3af62783
Yomar Miranda <mirandayomar@gmail.com>
2017-11-05 19:22:06 -0500

    Le agregamos un 'id' a cada uno de los elementos con los que vamos a trabajar desde Javascript, además de eso dentro de nuestro archivo Javascript creamos variables que hacen referencia a dichos elementos, también agregamos un escuchador a nuestro boton principal con el cual ocultamos nuestra ventana modal al pulsar sobre el

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+const $yesButton = document.getElementById('yesButton');
+const $overlay = document.getElementById('overlay');
+const $modal = document.getElementById('modal');
+
+$yesButton.addEventListener('click', (event) => {
+	//Con esto logramos que nuestra ventana modal desaparezca de nuestra ventana principal
+	$modal.style.animation = 'animationOut 1s forwards';
+})
