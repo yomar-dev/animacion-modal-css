45f2a64af65d0de42b341511d7a57d2e7b1136b4
Yomar Miranda <mirandayomar@gmail.com>
2017-11-05 19:34:13 -0500

    Cambiamos un poco los estilos, creamos una clase 'active' para nuestro overlay la cual vamos a remover cuando se pulse sobre el boton principal

--- js/modal.js
+++ js/modal.js
@@ -5,4 +5,5 @@ const $modal = document.getElementById('modal');
 $yesButton.addEventListener('click', (event) => {
 	//Con esto logramos que nuestra ventana modal desaparezca de nuestra ventana principal
 	$modal.style.animation = 'animationOut 1s forwards';
+	$overlay.classList.remove('active');
 })
